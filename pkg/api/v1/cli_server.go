07a5934dce98d41a94bc9e645e923172f6cc4d25
Marco <759374+mhelmich@users.noreply.github.com>
2021-12-11 21:21:12 -0800

    added comments to endpoints

--- pkg/api/v1/cli_server.go
+++ pkg/api/v1/cli_server.go
@@ -69,12 +69,15 @@ func (s *CliServer) Init(ctx context.Context, req *pb.InitRequest) (*pb.InitRepl
 	}, nil
 }
 
-// This will have to update a k8s manifest with a new version number of a container image.
+// This will have to create or update a haiku service manifest.
+// Those are relatively simple and be pulled in from the haiku operator.
+// The main attribute of those is the image url.
 func (s *CliServer) Deploy(ctx context.Context, req *pb.DeployRequest) (*pb.DeployReply, error) {
 	return nil, nil
 }
 
-// The env family of endpoints maybe gets stored as a k8s configmap. Who knows...
+// The env family of endpoints maybe gets stored as a k8s configmap.
+// The config map needs to be created in a particular namespace and with particular labels.
 func (s *CliServer) ListEnv(ctx context.Context, req *pb.ListEnvRequest) (*pb.ListEnvReply, error) {
 	return nil, nil
 }
@@ -88,6 +91,7 @@ func (s *CliServer) RemoveEnv(ctx context.Context, req *pb.RemoveEnvRequest) (*p
 }
 
 // This will have to create a k8s secret (and maybe patch that secret to the local service account).
+// As illustrated here: https://knative.dev/docs/serving/deploying-from-private-registry/
 func (s *CliServer) DockerLogin(ctx context.Context, req *pb.DockerLoginRequest) (*pb.DockerLoginReply, error) {
 	return nil, nil
 }
