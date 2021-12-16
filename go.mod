a0db5cfb6bdff8cc5d3d7581adbe1e66bbde5f3d
Marco <759374+mhelmich@users.noreply.github.com>
2021-12-15 22:09:16 -0800

    create docker secret service

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/go-logr/zerologr v1.2.1
 	github.com/golang/protobuf v1.5.2
 	github.com/lithammer/shortuuid/v3 v3.0.7
+	github.com/mhelmich/haiku-operator v0.0.0-20211214225724-703ece0f95da
 	github.com/rs/zerolog v1.26.0
 	google.golang.org/grpc v1.42.0
 	google.golang.org/protobuf v1.27.1
@@ -18,30 +19,31 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/google/go-cmp v0.5.5 // indirect
-	github.com/google/gofuzz v1.1.0 // indirect
-	github.com/google/uuid v1.2.0 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/googleapis/gnostic v0.5.5 // indirect
-	github.com/imdario/mergo v0.3.5 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/net v0.0.0-20210825183410-e898025ed96a // indirect
-	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f // indirect
-	golang.org/x/sys v0.0.0-20210831042530-f4d43177bf5e // indirect
+	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
+	golang.org/x/oauth2 v0.0.0-20211005180243-6b3c2da341f1 // indirect
+	golang.org/x/sys v0.0.0-20210917161153-d61c044b1678 // indirect
 	golang.org/x/term v0.0.0-20210615171337-6886f2dfbf5b // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/genproto v0.0.0-20210402141018-6c239bbf2bb1 // indirect
+	google.golang.org/genproto v0.0.0-20211016002631-37fc39342514 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 	k8s.io/klog/v2 v2.30.0 // indirect
 	k8s.io/kube-openapi v0.0.0-20211115234752-e816edb12b65 // indirect
 	k8s.io/utils v0.0.0-20210930125809-cb0fa318a74b // indirect
+	sigs.k8s.io/controller-runtime v0.10.0 // indirect
 	sigs.k8s.io/json v0.0.0-20211020170558-c049b76a60c6 // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.1.2 // indirect
-	sigs.k8s.io/yaml v1.2.0 // indirect
+	sigs.k8s.io/yaml v1.3.0 // indirect
 )
--- pkg/api/v1/cli_server.go
+++ pkg/api/v1/cli_server.go
@@ -7,10 +7,14 @@ import (
 	"github.com/go-logr/logr"
 	"github.com/mhelmich/haiku-api/pkg/api/v1/pb"
 	"github.com/mhelmich/haiku-api/pkg/requestid"
+	ho "github.com/mhelmich/haiku-operator/apis/entities/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
 )
@@ -25,8 +29,27 @@ func KubeConfigGetter(path string) clientcmd.KubeconfigGetter {
 	}
 }
 
+func newRestClient(config *rest.Config) (*rest.RESTClient, error) {
+	crdConfig := *config
+	crdConfig.ContentConfig.GroupVersion = &ho.GroupVersion
+	crdConfig.APIPath = "/apis"
+	crdConfig.NegotiatedSerializer = serializer.NewCodecFactory(scheme.Scheme)
+	crdConfig.UserAgent = rest.DefaultKubernetesUserAgent()
+	return rest.UnversionedRESTClientFor(&crdConfig)
+}
+
+// TODO: use fancy option pattern instead of this hack
 func NewCliServer(configPath string, logger logr.Logger) (*CliServer, error) {
-	config, err := clientcmd.BuildConfigFromKubeconfigGetter("", KubeConfigGetter(configPath))
+	var config *rest.Config
+	var err error
+	// register our CRDs with the client
+	ho.AddToScheme(scheme.Scheme)
+
+	if configPath == "" {
+		config, err = rest.InClusterConfig()
+	} else {
+		config, err = clientcmd.BuildConfigFromKubeconfigGetter("", KubeConfigGetter(configPath))
+	}
 	if err != nil {
 		return nil, err
 	}
@@ -36,17 +59,24 @@ func NewCliServer(configPath string, logger logr.Logger) (*CliServer, error) {
 		return nil, err
 	}
 
+	unversionedRestClient, err := newRestClient(config)
+	if err != nil {
+		return nil, err
+	}
+
 	return &CliServer{
-		k8sClient: k8sClient,
-		logger:    logger,
+		k8sClient:             k8sClient,
+		unversionedRestClient: unversionedRestClient,
+		logger:                logger,
 	}, nil
 }
 
 type CliServer struct {
 	pb.UnimplementedCliServiceServer
 
-	k8sClient *kubernetes.Clientset
-	logger    logr.Logger
+	k8sClient             *kubernetes.Clientset
+	unversionedRestClient *rest.RESTClient
+	logger                logr.Logger
 }
 
 // This will have to create a k8s namespace and likely more stuff.
@@ -95,5 +125,30 @@ func (s *CliServer) RemoveEnv(ctx context.Context, req *pb.RemoveEnvRequest) (*p
 // This will have to create a k8s secret (and maybe patch that secret to the local service account).
 // As illustrated here: https://knative.dev/docs/serving/deploying-from-private-registry/
 func (s *CliServer) DockerLogin(ctx context.Context, req *pb.DockerLoginRequest) (*pb.DockerLoginReply, error) {
-	return nil, nil
+	dl := &ho.DockerLogin{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "test-api",
+			Name:      "super-secret",
+		},
+		Spec: ho.DockerLoginSpec{
+			Server:   req.Server,
+			Username: req.Username,
+			Password: req.Password,
+			Email:    req.Email,
+		},
+	}
+	created := &ho.DockerLogin{}
+	err := s.unversionedRestClient.Post().
+		Resource("dockerlogins").
+		Namespace("test-api").
+		Body(dl).
+		Do(ctx).
+		Into(created)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.DockerLoginReply{
+		ID: string(created.UID),
+	}, nil
 }
